cc51844d7523ac399bdc6a64aa16cee046c21efc
Reddy <sahithi.reddy@capgemini.com>
2019-04-11 12:23:04 -0400

    update role based authorization logic

--- src/main/java/com/mycompany/store/repository/InvoiceRepository.java
+++ src/main/java/com/mycompany/store/repository/InvoiceRepository.java
@@ -1,6 +1,8 @@
 package com.mycompany.store.repository;
 
 import com.mycompany.store.domain.Invoice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,6 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-
+    Invoice findOneByIdAndOrderCustomerUserLogin(Long id, String login);
+    Page<Invoice> findAllByOrderCustomerUserLogin(String login, Pageable pageable);
 }
--- src/main/java/com/mycompany/store/repository/ProductOrderRepository.java
+++ src/main/java/com/mycompany/store/repository/ProductOrderRepository.java
@@ -1,6 +1,8 @@
 package com.mycompany.store.repository;
 
 import com.mycompany.store.domain.ProductOrder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,6 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface ProductOrderRepository extends JpaRepository<ProductOrder, Long> {
-
+    ProductOrder findOneByIdAndCustomerUserLogin(Long id, String login);
+    Page<ProductOrder> findAllByCustomerUserLogin(String login, Pageable pageable);
 }
